3eac29b99d87b9e59e95b499c138622de59e2184
hanseul <w0w1278@naver.com>
2022-08-09 13:08:09 +0900

    version edit

--- index.js
+++ index.js
@@ -1,59 +1,25 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-    if (request.method == "GET") {
-        const resp = await fetch(
-            "https://api.notion.com/v1/databases/a28191c709ea4407aa90e8d7d9880f11/query", {
-                headers: {
-                    Authorization: `Bearer ${NOTION_TOKEN}`,
-                    "Notion-Version": "2022-06-28"
-                },
-                method: "POST",
-            })
-        const data = await resp.json()
-        return new Response(JSON.stringify(data), {
-            headers: {
-                "Content-type": "application/json;charset=UTF-8",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-                "Access-Control-Max-Age": "86400",
-            }
-        })
-    }
-}
-
-
-
-// export default {
-//     async fetch(
-//         request : Request,
-//         env : Env,
-//         ctx : ExecutionContext
-//     ): Promise<Response> {
-//         if (request.method === "GET") {
-//             const data = await (
-//                 await fetch(
-//                     "https://api.notion.com/v1/databases/a28191c709ea4407aa90e8d7d9880f11/query",
-//                     {
-//                         headers: {
-//                             Authorization: `Bearer ${env.NOTION_TOKEN}`,
-//                             "Notion-Version": "2022-06-28",
-//                         },
-//                         method: "POST",
-//                     }
-//                 )
-//             ).json();
-//             return new Response(JSON.stringify(data), {
-//                 headers: {
-//                     "content-type": "application/json;charset=UTF-8",
-//                     "Access-Control-Allow-Origin": "*",
-//                     "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-//                     "Access-Control-Max-Age": "86400",
-//                 },
-//             });
-//         }
-//     },
-// };
-// djfksjdlkfjamdsfmx
+     if (request.method == "GET") {
+         const resp = await fetch(
+             "https://api.notion.com/v1/databases/a28191c709ea4407aa90e8d7d9880f11/query", {
+                 headers: {
+                     Authorization: `Bearer ${NOTION_TOKEN}`,
+                     "Notion-Version": "2021-05-11"
+                 },
+                 method: "POST",
+             })
+         const data = await resp.json()
+         return new Response(JSON.stringify(data), {
+             headers: {
+                 "Content-type": "application/json;charset=UTF-8",
+                 "Access-Control-Allow-Origin": "*",
+                 "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+                 "Access-Control-Max-Age": "86400",
+             }
+         })
+     }
+ }
